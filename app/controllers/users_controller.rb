638f90583368ef95118786dbb566a72dc64845c0
kedarnhegde <kedarnhegde@gmail.com>
2024-02-16 17:01:43 +0900

    link sign up to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Redirect or render something
+      flash[:success] = "Welcome to the Tweeter App!"
+      redirect_to @user
     else
       render 'new'
     end
