ddfcad7a791005caf8f3d548ff30cde3813b8858
kedarnhegde <kedarnhegde@gmail.com>
2024-02-27 15:30:27 +0900

    checkbox for remember me

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -29,6 +29,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path # Logout again (same user logged in from 2 tabs and logs out from one))
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
