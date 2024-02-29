fd98eadbed7443a2ec39aecdc359d030dc3edc23
kedarnhegde <kedarnhegde@gmail.com>
2024-02-29 15:34:45 +0900

    test: test for authentication

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,25 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:kedar)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
   end
 
+  test "should redirect to edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect to login when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name,
+                                              email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
 end
