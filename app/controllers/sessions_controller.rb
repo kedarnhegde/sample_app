4b3f3c7120f2eef5dc8a1dd0a6307cdfd679fe78
kedarnhegde <kedarnhegde@gmail.com>
2024-03-05 15:54:29 +0900

    handle account activation in web

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user 
       if user.authenticate(params[:session][:password])
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_to_prev user
+        if user.activated?
+          log_in user
+          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+          redirect_to_prev user
+        else
+          message = "Account not activated. "
+          message += "Check your email for the activation link."
+          flash[:warning] = message
+          redirect_to root_url
+        end
       else
         flash.now[:danger] = "Incorrect password for '#{params[:session][:email].downcase}' Please try again!"
         render 'new'
